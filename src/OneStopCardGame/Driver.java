e4fb89a57c77972c05eee51bff400a51d31267d7
Ivandynasty <ivanxie2021241932@gmail.com>
2019-04-09 22:30:23 -0500

    Deck and Driver

---
+++ src/OneStopCardGame/Driver.java
@@ -0,0 +1,12 @@
+package OneStop;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		CardGames controll = new CardGames();
+		controll.runGame(); 		
+	   
+		//gameControl controller= new gameControl();  
+		//controller.runGame();  	
+	}
+}
