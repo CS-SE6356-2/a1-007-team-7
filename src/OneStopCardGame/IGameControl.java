ba6e307d8b6514129ad0b2009bb50ea239f9e932
cmunongo99 <47436995+cmunongo99@users.noreply.github.com>
2019-04-09 22:49:42 -0500

    Adding IGameControl Class
    Adding IGameControl.java

---
+++ src/OneStopCardGame/IGameControl.java
@@ -0,0 +1,12 @@
+package OneStop;
+
+public interface IGameControl {
+	void playGame();
+	void runGame();
+	void init();
+}
+
+interface IGameView{
+	void display(String message);
+	char getInput();
+}
